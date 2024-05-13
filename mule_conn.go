93ff249c758b61c9e0e55d2fed57518d8b843491
root <root@vps3.colobu.com>
2024-05-13 09:21:31 +0800

    a workable version

--- mule_conn.go
+++ mule_conn.go
@@ -1,8 +1,6 @@
 package mule
 
 import (
-	"fmt"
-	"log"
 	"net"
 	"time"
 
@@ -24,18 +22,6 @@ type OptionFn func(*Conn)
 func WithLocalIP(localIP string) OptionFn {
 	return func(c *Conn) {
 		c.localIP = localIP
-	}
-}
-
-func WithLocalPort(localPort uint16) OptionFn {
-	return func(c *Conn) {
-		c.localPort = localPort
-	}
-}
-
-func WithRemotePort(remotePort uint16) OptionFn {
-	return func(c *Conn) {
-		c.remotePort = remotePort
 	}
 }
 
@@ -67,12 +53,10 @@ func WithIPv4Flag(flag IPv4Flag) OptionFn {
 // You can use it to send UDP packets to multiple remote servers with unreachable ports.
 // Then you receive ICMP Destination/Port Unreachable packets from the remote servers.
 type Conn struct {
-	packetConn net.PacketConn
-	rawConn    *ipv4.RawConn
+	sendConn *ipv4.RawConn
+	recvConn *icmp.PacketConn
 
-	localIP    string
-	localPort  uint16
-	remotePort uint16
+	localIP string
 
 	timeout time.Duration
 
@@ -93,62 +77,73 @@ func New(opts ...OptionFn) (*Conn, error) {
 		conn.localIP = "127.0.0.1"
 	}
 
-	if conn.remotePort == 0 {
-		conn.remotePort = 65535
-	}
-
 	// begin to listen
 	err := conn.listen()
 	if err != nil {
 		return nil, err
 	}
 
-	rawConn, err := ipv4.NewRawConn(conn.packetConn)
-	if err != nil {
-		_ = conn.Close()
-		return nil, err
-	}
-	conn.rawConn = rawConn
-
 	return conn, nil
 }
 
 func (c *Conn) Close() error {
-	return c.packetConn.Close()
+	if c.sendConn != nil {
+		_ = c.sendConn.Close()
+	}
+
+	if c.recvConn != nil {
+		return c.recvConn.Close()
+	}
+
+	return nil
 }
 
 func (c *Conn) listen() error {
-	conn, err := net.ListenPacket("ip4:udp", c.localIP)
+	pconn, err := net.ListenPacket("ip4:udp", c.localIP)
 	if err != nil {
 		return err
 	}
-	c.packetConn = conn
+	rawConn, err := ipv4.NewRawConn(pconn)
+	if err != nil {
+		_ = pconn.Close()
+		return err
+	}
+	c.sendConn = rawConn
+
+	recvConn, err := icmp.ListenPacket("ip4:1", c.localIP)
+	if err != nil {
+		return err
+	}
+	c.recvConn = recvConn
 
 	return nil
 }
 
 // ReadFromIP reads ICMP data from the remote server.
-func (c *Conn) ReadFromIP() ([]byte, string, uint16, error) {
+func (c *Conn) ReadFrom() (string, uint16, uint16, error) {
 	buf := make([]byte, 65535)
 
 	start := time.Now()
+
+loop:
 	for {
 		if c.timeout > 0 {
-			c.rawConn.SetReadDeadline(time.Now().Add(c.timeout))
+			c.recvConn.SetReadDeadline(time.Now().Add(c.timeout))
 		}
-		header, payload, _, err := c.rawConn.ReadFrom(buf)
+
+		n, _, err := c.recvConn.ReadFrom(buf)
 		if err != nil {
-			return nil, "", 0, err
+			return "", 0, 0, err
 		}
 
 		if time.Since(start) > c.timeout {
-			return nil, "", 0, fmt.Errorf("timeout")
+			return "", 0, 0, err
 		}
 
 		// 解析 ICMP 报文
-		msg, err := icmp.ParseMessage(protocolICMP, payload)
+		msg, err := icmp.ParseMessage(protocolICMP, buf[:n])
 		if err != nil {
-			log.Fatal(err)
+			return "", 0, 0, err
 		}
 
 		// 打印结果
@@ -156,36 +151,48 @@ func (c *Conn) ReadFromIP() ([]byte, string, uint16, error) {
 		case ipv4.ICMPTypeDestinationUnreachable:
 			dstUnreach, ok := msg.Body.(*icmp.DstUnreach)
 			if !ok {
-				continue
+				continue loop
 			}
 
-			// 解析dstUnreach， 它是一个UDP的包头, 打印源目端口
-			udpPacket := gopacket.NewPacket(dstUnreach.Data, layers.LayerTypeUDP, gopacket.Default)
-			udpLayer := udpPacket.Layer(layers.LayerTypeUDP)
+			packet := gopacket.NewPacket(dstUnreach.Data, layers.LayerTypeIPv4, gopacket.Default)
+			if packet == nil {
+				continue loop
+			}
+
+			// 获取IP层
+			ipLayer := packet.Layer(layers.LayerTypeIPv4)
+			if ipLayer == nil {
+				continue loop
+			}
+
+			ip, _ := ipLayer.(*layers.IPv4)
+
+			// 获取UDP层
+			udpLayer := packet.Layer(layers.LayerTypeUDP)
 			if udpLayer == nil {
-				continue
+				continue loop
 			}
+			udp, _ := udpLayer.(*layers.UDP)
 
-			udpHeader := udpLayer.(*layers.UDP)
-			return dstUnreach.Data, header.Dst.String(), uint16(udpHeader.DstPort), nil
+			return ip.DstIP.String(), uint16(udp.SrcPort), uint16(udp.DstPort), nil
 
 		default:
-			fmt.Printf("Unexpected ICMP message type: %v\n", msg.Type)
+
 		}
 	}
 }
 
 // WriteToIP writes UDP data to the destination.
-func (c *Conn) WriteToIP(payload []byte, remoteIP string) (int, error) {
-	data, _ := c.encodeIPPacket(remoteIP, payload)
+func (c *Conn) WriteToIP(payload []byte, remoteIP string, localPort, remotePort uint16) (int, error) {
+	data, _ := c.encodeIPPacket(remoteIP, localPort, remotePort, payload)
 
 	if c.timeout > 0 {
-		c.rawConn.SetDeadline(time.Now().Add(c.timeout))
+		c.sendConn.SetDeadline(time.Now().Add(c.timeout))
 	}
-	return c.rawConn.WriteToIP(data, &net.IPAddr{IP: net.ParseIP(remoteIP)})
+	return c.sendConn.WriteToIP(data, &net.IPAddr{IP: net.ParseIP(remoteIP)})
 }
 
-func (c *Conn) encodeIPPacket(dstIP string, payload []byte) ([]byte, error) {
+func (c *Conn) encodeIPPacket(dstIP string, localPort, remotePort uint16, payload []byte) ([]byte, error) {
 	ip := &layers.IPv4{
 		SrcIP:    net.ParseIP(c.localIP),
 		DstIP:    net.ParseIP(dstIP),
@@ -197,8 +204,8 @@ func (c *Conn) encodeIPPacket(dstIP string, payload []byte) ([]byte, error) {
 	}
 
 	udp := &layers.UDP{
-		SrcPort: layers.UDPPort(c.localPort),
-		DstPort: layers.UDPPort(c.remotePort),
+		SrcPort: layers.UDPPort(localPort),
+		DstPort: layers.UDPPort(remotePort),
 	}
 	udp.SetNetworkLayerForChecksum(ip)
 
--- mule_conn_test.go
+++ mule_conn_test.go
@@ -1,6 +1,7 @@
 package mule
 
 import (
+	"errors"
 	"net"
 	"testing"
 	"time"
@@ -10,21 +11,69 @@ import (
 )
 
 func TestConn(t *testing.T) {
-	conn, err := net.Dial("tcp", "bing.com:80")
+	conn, err := net.Dial("tcp4", "bing.com:80")
 	require.NoError(t, err)
 	localIP := conn.LocalAddr().(*net.TCPAddr).IP.String()
 	_ = conn.Close()
 
-	muleConn, err := New(WithLocalIP(localIP), WithTimeout(30*time.Second), WithRemotePort(65535))
-	require.NoError(t, err)
-
-	n, err := muleConn.WriteToIP([]byte("GET / HTTP/1.1\r\nHost: bing.com\r\n\r\n"), "114.114.114.114")
-	require.NoError(t, err)
-	assert.Greater(t, n, 0)
-
-	payload, dstIP, dstPort, err := muleConn.ReadFromIP()
+	remoteIP, err := getLastIP(localIP)
 	assert.NoError(t, err)
-	assert.NotEmpty(t, payload)
-	assert.Equal(t, dstIP, "114.114.114.114")
+
+	muleConn, err := New(WithLocalIP(localIP), WithTimeout(10*time.Second))
+	require.NoError(t, err)
+
+	go func() {
+		time.Sleep(time.Second)
+		n, err := muleConn.WriteToIP([]byte("test"), remoteIP, 8972, 65535)
+		require.NoError(t, err)
+		assert.Greater(t, n, 0)
+	}()
+
+	dstIP, srcPort, dstPort, err := muleConn.ReadFrom()
+	assert.NoError(t, err)
+	assert.Equal(t, srcPort, uint16(8972))
 	assert.Equal(t, dstPort, uint16(65535))
+	assert.Equal(t, dstIP, remoteIP)
+}
+
+func getLastIP(ipAddr string) (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		ipNet, ok := addr.(*net.IPNet)
+		if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+			// 找到一个非本地环回的IPv4地址
+			ip := ipNet.IP.To4()
+			if ip.String() != ipAddr {
+				continue
+			}
+
+			mask := ipNet.Mask
+			lastIP := calculateLastIP(ip, mask)
+			return lastIP.String(), nil
+		}
+	}
+
+	return "", errors.New("not found")
+}
+
+func calculateLastIP(ip net.IP, mask net.IPMask) net.IP {
+	ipUint32 := uint32(ip[0])<<24 | uint32(ip[1])<<16 | uint32(ip[2])<<8 | uint32(ip[3])
+	maskUint32 := uint32(mask[0])<<24 | uint32(mask[1])<<16 | uint32(mask[2])<<8 | uint32(mask[3])
+
+	networkPrefixUint32 := ipUint32 & maskUint32
+	// firstIPUint32 := networkPrefixUint32 + 1
+	lastIPUint32 := networkPrefixUint32 | ^maskUint32
+	lastIPUint32 = lastIPUint32 - 1 // remove broadcast address
+
+	lastIP := make(net.IP, 4)
+	lastIP[0] = byte(lastIPUint32 >> 24)
+	lastIP[1] = byte(lastIPUint32 >> 16)
+	lastIP[2] = byte(lastIPUint32 >> 8)
+	lastIP[3] = byte(lastIPUint32)
+
+	return lastIP
 }
